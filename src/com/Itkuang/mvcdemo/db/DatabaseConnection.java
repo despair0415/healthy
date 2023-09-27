95907fe2b22ee56955c66cfced7c3d85eea0d16b
healthy <despair01@qq.com>
2023-09-27 17:14:11 +0800

    9.27

--- src/com/Itkuang/mvcdemo/db/DatabaseConnection.java
+++ src/com/Itkuang/mvcdemo/db/DatabaseConnection.java
@@ -2,8 +2,8 @@ package com.Itkuang.mvcdemo.db ;
 import java.sql.* ;
 public class DatabaseConnection {
 	private static final String DBDRIVER = "com.mysql.cj.jdbc.Driver" ;
- 	private static final String DBURL = "jdbc:mysql://localhost:3306/smile?allowPublicKeyRetrieval=true&useSSL=false&&serverTimezone=GMT" ;
-	private static final String DBUSER = "root";
+ 	private static final String DBURL = "jdbc:mysql://1.94.26.136:3306/smile?allowPublicKeyRetrieval=true&useSSL=false&&serverTimezone=GMT" ;
+	private static final String DBUSER = "admin";
 	private static final String DBPASSWORD = "123456";
 	private Connection conn = null ;
 	public DatabaseConnection() throws Exception{
